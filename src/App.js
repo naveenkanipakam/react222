b306ccbd9b88d2e600b1567552813423ee34f57f
naveenkanipakam <naveenpillai015@gmail.com>
2023-06-02 16:07:34 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from "./components/header/header";
+import Cards from "./components/cards/cards";
+import Footer from "./components/footer/footer";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Cards />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/footer/footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+import styles from "./footer.module";
+
+const footer = () => {
+  return <footer className={styles.footer}></footer>;
+  
+};
+
+export default footer;
